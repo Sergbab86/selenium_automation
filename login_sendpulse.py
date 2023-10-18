54af0591324745009834ac07352f4070cb1cde45
sb <username@example.com>
2023-10-18 22:24:42 +0300

    refactored README

--- login_sendpulse.py
+++ login_sendpulse.py
@@ -37,7 +37,7 @@ sleep(2.5)
 # Click on captcha and finish log in process
 driver.switch_to.frame(driver.find_elements(By.TAG_NAME, "iframe")[0])
 driver.find_element(By.ID, 'recaptcha-anchor').click()
-wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[class="recaptcha-checkbox-checkmark"]')))
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.recaptcha-checkbox-checkmark')))
 driver.switch_to.default_content()
 sleep(1)
 driver.find_element(By.CSS_SELECTOR, 'button[name="submit"]').click()
@@ -49,22 +49,22 @@ driver.find_element(By.CSS_SELECTOR, 'a[class="btn btn-create"]').click()
 wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[onclick="addAddressBook();"]')))
 driver.find_element(By.CSS_SELECTOR, 'button[onclick="addAddressBook();"]').click()
 
-wait.until(EC.element_to_be_clickable((By.XPATH, '//span[@class="label label-as-link label-yellow arrow_box"]')))
-driver.find_element(By.XPATH, '//span[@class="label label-as-link label-yellow arrow_box"]').click()
+wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.label label-as-link label-yellow arrow_box')))
+driver.find_element(By.CSS_SELECTOR, 'span."label label-as-link label-yellow arrow_box"').click()
 
 email_form = driver.find_element(By.CSS_SELECTOR, 'textarea#emailsList')
 for email in random_emails:
     email_form.send_keys(email, Keys.ENTER)
 
-driver.find_element(By.CSS_SELECTOR, 'input[value="Upload"][name="submit_text"]').click()
+driver.find_element(By.CSS_SELECTOR, 'input[value="Upload"],[name="submit_text"]').click()
 wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#status-description > b:nth-child(1)')))
 
 # Delete created email list
 driver.find_element(By.CSS_SELECTOR, 'a.active > span:nth-child(2)').click()
-driver.find_element(By.CSS_SELECTOR, 'span[class="glyphicon glyphicon-option-horizontal"]').click()
+driver.find_element(By.CSS_SELECTOR, 'span."glyphicon glyphicon-option-horizontal"]').click()
 driver.find_element(By.XPATH, '(//a[contains(text(),"addressBookDeleteDialog")])').click()
-wait.until(EC.element_to_be_clickable((By.XPATH, '(//button[contains(text(),"addressBookDelete")])')))
-driver.find_element(By.XPATH, '(//button[contains(text(),"addressBookDelete")])').click()
+wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '(button:contains("addressBookDelete"))')))
+driver.find_element(By.CSS_SELECTOR, '(button:contains("addressBookDelete"))').click()
 
 # Log out
 wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'img-circle')))
