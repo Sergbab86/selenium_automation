0d13d5988871f7e0cc83bb6627facae94de1e8f9
sb <s.babyak12@gmail.com>
2023-10-24 19:36:53 +0300

    adde locators

--- locators.py
+++ locators.py
@@ -1,7 +1,14 @@
 
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators(object):
     """
     A class for main page locators
+    """
+
+
+class  ResultPageLocators(object):
+    """
+    A class for result page locators
     """
